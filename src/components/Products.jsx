8c6886a697e7febb7e8f764153e49e64aaff16a3
emmanuelhashy <emmanuelhashy@gmail.com>
2024-09-27 09:34:12 +0100

    .

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,7 +3,7 @@ import Product from './Product';
 
 const Products = ({ products }) => {
   return (
-    <section className="max-w-7xl mx-auto my-20">
+    <section className="max-w-7xl mx-auto px-6 my-20">
       <div className="text-center">
         <h2 className="text-3xl font-semibold">New Products</h2>
         <div className="w-28 h-1 bg-orange-500 mt-3 mx-auto"></div>
